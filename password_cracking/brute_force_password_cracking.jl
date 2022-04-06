e90567c6372d1d3a77abb77b07b09ef2aad91bfc
dianabarros <diana.ab@live.com>
2022-04-06 17:19:24 -0300

    Making benchmark

--- password_cracking/brute_force_password_cracking.jl
+++ password_cracking/brute_force_password_cracking.jl
@@ -2,19 +2,21 @@ using MD5
 using FoldsThreads
 using FLoops
 using Base.Threads
+using BenchmarkTools
 
-const MAX = 10
+mutable struct BenchmarkResults
+    loop_tasks::Vector{Vector{Vector{Int64}}}
+    suite::BenchmarkGroup
+end
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 thread_tasks = [String[] for _ in 1:nthreads()]
-pw = ""
 
 function brute_force(hash1, str, i, n)
     global letters
     if i == n + 1
         hash2 = md5(String(str))
-        # @show String(str)
         return hash1 == hash2
     end
     for letter in letters 
@@ -26,63 +28,10 @@ function brute_force(hash1, str, i, n)
     return false
 end
 
-# works in parallel but doesnt seem to interrupt
-# flow when result is found
-# function brute_force_parallel(hash1, str, i, n)
-#     global letters, thread_tasks, pw
-#     push!(thread_tasks[threadid()], String(str))
-#     if i == n + 1
-#         hash2 = md5(String(str))
-#         # @show String(str)
-#         if hash1 == hash2
-#             @show String(str)
-#         end
-#         return hash1 == hash2
-#     end
-#     @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters 
-#         new_str = str
-#         new_str[i] = letter 
-#         if brute_force_parallel(hash1, new_str, i + 1, n)
-#             return true
-#         end
-#     end
-#     return false
-# end
-
-# hash1_str = "be5d75fa67ef370e98b3d3611c318156"
-
-function crack_password(hash1_str::String)
-    hash1 = hex2bytes(hash1_str)
-    str = []
-    found =  false
-    for i in 2:8
-        str = fill('\0',i)
-        if brute_force(hash1, str, 1, i)
-            found = true
-            break
-        end
-    end
-    if found
-        return String(str)
-    end
-end
-
-function crack_password_parallel(hash1_str::String)
-    hash1 = hex2bytes(hash1_str)
-    str = []
-    for i in 2:3
-        str = fill('\0',i)
-        if brute_force_parallel(hash1)
-            break
-        end
-    end
-end
-
-
-function brute_force_parallel(hash1)
+function brute_force_parallel(hash1::Vector{UInt8}, ex::FoldsThreads.FoldsBase.Executor)
     lk = ReentrantLock()
     found = nothing
-    @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+    @floop ex for letter in letters if isnothing(found)
         local_str = fill('\0',1)
         local_str[1] = letter
         hash2 = md5(String(local_str))
@@ -95,7 +44,7 @@ function brute_force_parallel(hash1)
     end end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',2)
             local_str[1] = letter
             for letter in letters
@@ -112,7 +61,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',3)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -132,7 +81,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',4)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -155,7 +104,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',5)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -181,7 +130,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',6)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -210,7 +159,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',7)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -242,7 +191,7 @@ function brute_force_parallel(hash1)
     end
 
     if isnothing(found)
-        @floop DepthFirstEx(basesize = div(length(letters),4)) for letter in letters if isnothing(found)
+        @floop ex for letter in letters if isnothing(found)
             local_str = fill('\0',8)
             local_str[1] = letter
             for letter in letters if isnothing(found)
@@ -276,4 +225,249 @@ function brute_force_parallel(hash1)
         end end
     end
     return found
+end
+
+function debug_brute_force_parallel(hash1::Vector{UInt8}, ex::FoldsThreads.FoldsBase.Executor, loop_tasks, suite)
+    lk = ReentrantLock()
+    found = nothing
+    suite["loop_1"] = @benchmarkable begin
+        @floop $ex for letter in $letters if isnothing($found)
+            local_str = fill('\0',1)
+            local_str[1] = letter
+            hash2 = md5(String(local_str))
+            if $hash1 == hash2
+                lock($lk) do
+                    $found = String(local_str)
+                end
+                break
+            end
+        end end
+    end
+
+    suite["loop_2"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',2)
+                local_str[1] = letter
+                for letter in $letters
+                    local_str[2] = letter
+                    hash2 = md5(String(local_str))
+                    if $hash1 == hash2
+                        lock($lk) do
+                            $found = String(local_str)
+                        end
+                        break
+                    end
+                end
+            end end   
+        end
+    end
+
+    suite["loop_3"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',3)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters
+                        local_str[3] = letter
+                        hash2 = md5(String(local_str))
+                        if $hash1 == hash2
+                            lock($lk) do
+                                $found = String(local_str)
+                            end
+                            break
+                        end
+                    end
+                end end
+            end end
+        end
+    end
+
+    suite["loop_4"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',4)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters if isnothing($found)
+                        local_str[3] = letter
+                        for letter in $letters
+                            local_str[4] = letter
+                            hash2 = md5(String(local_str))
+                            if $hash1 == hash2
+                                lock($lk) do
+                                    $found = String(local_str)
+                                end
+                                break
+                            end
+                        end
+                    end end
+                end end
+            end end
+        end
+    end
+
+    suite["loop_5"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',5)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters if isnothing($found)
+                        local_str[3] = letter
+                        for letter in $letters if isnothing($found)
+                            local_str[4] = letter
+                            for letter in $letters
+                                local_str[5] = letter
+                                hash2 = md5(String(local_str))
+                                if $hash1 == hash2
+                                    lock($lk) do
+                                        $found = String(local_str)
+                                    end
+                                    break
+                                end
+                            end
+                        end end
+                    end end
+                end end
+            end end
+        end
+    end
+
+    suite["loop_6"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',6)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters if isnothing($found)
+                        local_str[3] = letter
+                        for letter in $letters if isnothing($found)
+                            local_str[4] = letter
+                            for letter in $letters if isnothing($found)
+                                local_str[5] = letter
+                                for letter in $letters
+                                    local_str[6] = letter
+                                    hash2 = md5(String(local_str))
+                                    if $hash1 == hash2
+                                        lock($lk) do
+                                            $found = String(local_str)
+                                        end
+                                        break
+                                    end
+                                end
+                            end end
+                        end end
+                    end end
+                end end
+            end end
+        end
+    end
+
+    suite["loop_7"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',7)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters if isnothing($found)
+                        local_str[3] = letter
+                        for letter in $letters if isnothing($found)
+                            local_str[4] = letter
+                            for letter in $letters if isnothing($found)
+                                local_str[5] = letter
+                                for letter in $letters if isnothing($found)
+                                    local_str[6] = letter
+                                    for letter in $letters
+                                        local_str[7] = letter
+                                        hash2 = md5(String(local_str))
+                                        if $hash1 == hash2
+                                            lock($lk) do
+                                                $found = String(local_str)
+                                            end
+                                            break
+                                        end
+                                    end
+                                end end
+                            end end
+                        end end
+                    end end
+                end end
+            end end
+        end
+    end
+
+    suite["loop_8"] = @benchmarkable begin
+        if isnothing($found)
+            @floop $ex for letter in $letters if isnothing($found)
+                local_str = fill('\0',8)
+                local_str[1] = letter
+                for letter in $letters if isnothing($found)
+                    local_str[2] = letter
+                    for letter in $letters if isnothing($found)
+                        local_str[3] = letter
+                        for letter in $letters if isnothing($found)
+                            local_str[4] = letter
+                            for letter in $letters if isnothing($found)
+                                local_str[5] = letter
+                                for letter in $letters if isnothing($found)
+                                    local_str[6] = letter
+                                    for letter in $letters if isnothing($found)
+                                        local_str[7] = letter
+                                        for letter in $letters
+                                            local_str[8] = letter
+                                            hash2 = md5(String(local_str))
+                                            if $hash1 == hash2
+                                                lock($lk) do
+                                                    $found = String(local_str)
+                                                end
+                                                break
+                                            end
+                                        end
+                                    end end
+                                end end
+                            end end
+                        end end
+                    end end
+                end end
+            end end
+        end
+    end
+    return found
+end
+
+function crack_password(hash1_str::String)
+    hash1 = hex2bytes(hash1_str)
+    str = []
+    found =  false
+    for i in 2:8
+        str = fill('\0',i)
+        if brute_force(hash1, str, 1, i)
+            found = true
+            break
+        end
+    end
+    if found
+        return String(str)
+    end
+end
+
+function crack_password_parallel(kwargs::NamedTuple{T}) where T
+    hash1 = hex2bytes(kwargs.hash1_str)
+    return brute_force_parallel(hash1, kwargs.ex)
+end
+
+function debug_crack_password_parallel(kwargs::NamedTuple{T}) where T
+    loop_tasks = [[Int64[] for _ in 1:nthreads()] for _ in 1:8]
+    suite = BenchmarkGroup()
+    hash1 = hex2bytes(kwargs.hash1_str)
+    debug_brute_force_parallel(hash1, kwargs.ex, loop_tasks, suite)
+    tune!(suite)
+    return BenchmarkResults(loop_tasks, suite)
 end
