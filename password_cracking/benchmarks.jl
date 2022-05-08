5eec504aac4f52c81f9f4b6633d50ab3976b78a2
dianabarros <diana.ab@live.com>
2022-05-08 17:45:29 -0300

    pw cracking benchmark

---
+++ password_cracking/benchmarks.jl
@@ -0,0 +1,90 @@
+using DataFrames, CSV
+
+include("brute_force_password_cracking.jl")
+
+inputs = Dict(
+    4 => Dict(
+        "FAZE" => "be5d75fa67ef370e98b3d3611c318156",
+        "ENCA" => "9b124d075302f51d5412a1fbe6d83ac9",
+        "2ABR" => "4d602f3e818101225f216634e31cd8ed",
+        "GRAM" => "6f4862a9a705c7dea77f97b43ebfb7df",
+        "ZRNA" => "7d32ec88925bf045d8d337f9f083de5f",
+        "0OLA" => "de04ff86e862cd3e380657323e303150",
+        "PESS" => "9ec368597e78313f9171c7695d962057",
+        "ALMA" => "3ebddf9804c556ddfd0e86eb23f59f51",
+        "MADE" => "8551d9042765fcb1f22831ba010aa501",
+        "TLES" => "c6a77b48edce59d77093f6d6e6f3504f"
+    ),
+    5 => Dict(
+        "ANIMA" => "9cbbf96d1973a60adebbb153f64b48f6",
+        "FANTA" => "452e37b78259f1ffc130ad85663f4c40",
+        "GUERR" => "467fe75c655020c476f0199d47e883cd",
+        "TRUQE" => "31012a20977110286088c4e7af1a884e",
+        "VESKA" => "f9e9eb919de9df008f1ef4d4bbf31808",
+        "PEDRE" => "e10710d272e22dfe91517b8ccc7def0e",
+        "2VOPA" => "6ea4c5b271e879afa7aaa5592b72aaeb",
+        "7LOMA" => "b56008e0f06e5f3a2f5156021a9b4e2b",
+        "MASKA" => "39c2b07a337c0c7474839ce283c2d20e",
+        "ZORRA" => "a549e5242472175ae8facd9f1d242150"
+    ),
+    6 => Dict(
+        "GEOMET" => "34799a12a6ef24ef95a0f3179ac3c78d",
+        "SANGUE" => "0fe2df8cc3c185033bb7b9906edae7d1",
+        "ANGUES" => "05887c6b7ee5e63a089bd289d5aa3239",
+        "TRAJES" => "f5b59ec5143d666b7239fcb29a51d816",
+        "LUTASS" => "9cba3ac4fcfd9e5abd06ece9b8b33db5",
+        "OUTONO" => "e3d0d777e0ce1b9ed8f68360879f4606",
+        "CLIENT" => "ef10c650df47bffd6399e5e78da2a9b1",
+        "9ASKAS" => "79973eac0daf545f164a4c0d71f99f58",
+        "2POCAS" => "5c6a9d86bc6aad97122178e36ec053f6",
+        "ZEBRAS" => "9c5cd8d2ad3478c090b050c23aca5d43"
+    )
+)
+
+funcs = [debug_brute_force, debug_brute_force_threads, debug_brute_force_floop]
+
+basesizes = [div(length(letters), nthreads())]
+
+executors = [ThreadedEx, WorkStealingEx, DepthFirstEx, TaskPoolEx, NondeterministicEx]
+
+check_sequential = true
+
+runs = []
+
+for pw_size in keys(inputs)
+    for (pw, hash_str) in inputs[pw_size]
+        for func in funcs
+            for exec in executors
+                for basesize in basesizes
+                    run = (f=func, pw=pw, hash_str=hash_str, ex=exec, basesize=basesize, check_sequential=check_sequential)
+                    push!(runs, run)
+                end
+            end
+        end
+    end
+end
+
+iterations = 1
+
+df = DataFrame(func=String[], input=String[], executor=String[], basesize=Int64[], total_time=Float64[])
+df_file_name = "pw_cracking_results.csv"
+
+task_distribution = []
+
+for run in runs
+    for it in 1:iterations
+        @show run
+        it_dist = Dict()
+        bench_sample = debug_crack_password(
+            run.f, run.hash_str, ex=run.ex(basesize=run.basesize), check_sequential=run.check_sequential
+        )
+        it_dist[it] = bench_sample.loop_tasks
+        push!(df, (func=String(Symbol(run.f)), input=run.pw, executor=String(Symbol(run.ex)), basesize=run.basesize, total_time=bench_sample.suite["app"][2]))
+        CSV.write(df_file_name, df)
+    end
+    push!(task_distribution, (run=run, dist=it_dist))
+end
+
+open("pw_craking_task_distribution.txt", "w") do io
+    print(io, task_distribution)
+end
