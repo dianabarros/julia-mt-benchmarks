07d496fc6c04e9ffeeef88b0fc5bfdda57635199
dianabarros <diana.ab@live.com>
2022-05-18 20:18:54 -0300

    Task time measurement

--- mutually_friendly_numbers/friendly_numbers.jl
+++ mutually_friendly_numbers/friendly_numbers.jl
@@ -4,7 +4,7 @@ using Base.Threads
 
 mutable struct BenchmarkSample
     task_distribution::Vector{Vector{Int64}}
-    suite::Dict{String,NamedTuple}
+    suite::Dict{String,Union{NamedTuple,Vector{Vector{NamedTuple}}}}
     correct_results::Union{Bool,Nothing}
 end
 BenchmarkSample(task_distribution, suite) = BenchmarkSample(task_distribution, suite, nothing)
@@ -152,50 +152,50 @@ function friendly_numbers_threads(start::Int64, stop::Int64, ex::FoldsThreads.Fo
 end
 
 function debug_friendly_numbers(
-    start::Int64, stop::Int64; ex::Union{FoldsThreads.FoldsBase.Executor,Nothing}=nothing, 
-    task_distribution::Union{Vector{Vector{Int64}},Nothing}=nothing,
-    suite::Union{Dict{T},Nothing}=nothing
-) where T
-last = stop - start + 1
-the_num = zeros(Int64, last)
-num = zeros(Int64, last)
-den = zeros(Int64, last)
-result_a = zeros(Int64, last)
-result_b = zeros(Int64, last)
-for i in start:stop
-    ii = i - start
-    sum = 1 + i
-    the_num[ii+1] = i
-    done = i
-    factor = 2
+        start::Int64, stop::Int64; ex::Union{FoldsThreads.FoldsBase.Executor,Nothing}=nothing, 
+        task_distribution::Union{Vector{Vector{Int64}},Nothing}=nothing,
+        suite::Union{Dict{T},Nothing}=nothing
+    ) where T
+    last = stop - start + 1
+    the_num = zeros(Int64, last)
+    num = zeros(Int64, last)
+    den = zeros(Int64, last)
+    result_a = zeros(Int64, last)
+    result_b = zeros(Int64, last)
+    for i in start:stop
+        ii = i - start
+        sum = 1 + i
+        the_num[ii+1] = i
+        done = i
+        factor = 2
 
-    while (factor < done)
-        if i % factor == 0
-            sum += factor + div(i, factor)
-            done = div(i, factor) 
-            if done == factor
-                sum -= factor
+        while (factor < done)
+            if i % factor == 0
+                sum += factor + div(i, factor)
+                done = div(i, factor) 
+                if done == factor
+                    sum -= factor
+                end
+            end
+            factor += 1
+        end
+        num[ii+1] = sum
+        den[ii+1] = i
+        n = gcd(num[ii+1], den[ii+1])
+        num[ii+1] = div(num[ii+1], n)
+        den[ii+1] = div(den[ii+1], n)
+    end
+    n_result = 0
+    for i in 0:last-1
+        for j in i+1:last-1
+            if num[i+1] == num[j+1] && den[i+1] == den[j+1]
+                result_a[n_result+1] = the_num[i+1]
+                result_b[n_result+1] = the_num[j+1]
+                n_result += 1
             end
         end
-        factor += 1
     end
-    num[ii+1] = sum
-    den[ii+1] = i
-    n = gcd(num[ii+1], den[ii+1])
-    num[ii+1] = div(num[ii+1], n)
-    den[ii+1] = div(den[ii+1], n)
-end
-n_result = 0
-for i in 0:last-1
-    for j in i+1:last-1
-        if num[i+1] == num[j+1] && den[i+1] == den[j+1]
-            result_a[n_result+1] = the_num[i+1]
-            result_b[n_result+1] = the_num[j+1]
-            n_result += 1
-        end
-    end
-end
-return result_a, result_b
+    return result_a, result_b
 end
 
 function debug_friendly_numbers_floop(
@@ -212,27 +212,30 @@ function debug_friendly_numbers_floop(
     suite["loop"] = @timed begin
         @floop ex for i in start:stop
             push!(task_distribution[threadid()], i) 
-            ii = i - start
-            sum = 1 + i
-            the_num[ii+1] = i
-            done = i
-            factor = 2
+            threadinfo = @timed begin
+                ii = i - start
+                sum = 1 + i
+                the_num[ii+1] = i
+                done = i
+                factor = 2
 
-            while (factor < done)
-                if i % factor == 0
-                    sum += factor + div(i, factor)
-                    done = div(i, factor) 
-                    if done == factor
-                        sum -= factor
+                while (factor < done)
+                    if i % factor == 0
+                        sum += factor + div(i, factor)
+                        done = div(i, factor) 
+                        if done == factor
+                            sum -= factor
+                        end
                     end
+                    factor += 1
                 end
-                factor += 1
+                num[ii+1] = sum
+                den[ii+1] = i
+                n = gcd(num[ii+1], den[ii+1])
+                num[ii+1] = div(num[ii+1], n)
+                den[ii+1] = div(den[ii+1], n)
             end
-            num[ii+1] = sum
-            den[ii+1] = i
-            n = gcd(num[ii+1], den[ii+1])
-            num[ii+1] = div(num[ii+1], n)
-            den[ii+1] = div(den[ii+1], n)
+            push!(suite["task"][threadid()], threadinfo)
         end
     end
     n_result = 0
@@ -262,27 +265,30 @@ function debug_friendly_numbers_threads(
     suite["loop"] = @timed begin
         @threads for i in start:stop
             push!(task_distribution[threadid()], i)
-            ii = i - start
-            sum = 1 + i
-            the_num[ii+1] = i
-            done = i
-            factor = 2
+            threadinfo = @timed begin
+                ii = i - start
+                sum = 1 + i
+                the_num[ii+1] = i
+                done = i
+                factor = 2
 
-            while (factor < done)
-                if i % factor == 0
-                    sum += factor + div(i, factor)
-                    done = div(i, factor) 
-                    if done == factor
-                        sum -= factor
+                while (factor < done)
+                    if i % factor == 0
+                        sum += factor + div(i, factor)
+                        done = div(i, factor) 
+                        if done == factor
+                            sum -= factor
+                        end
                     end
+                    factor += 1
                 end
-                factor += 1
+                num[ii+1] = sum
+                den[ii+1] = i
+                n = gcd(num[ii+1], den[ii+1])
+                num[ii+1] = div(num[ii+1], n)
+                den[ii+1] = div(den[ii+1], n)
             end
-            num[ii+1] = sum
-            den[ii+1] = i
-            n = gcd(num[ii+1], den[ii+1])
-            num[ii+1] = div(num[ii+1], n)
-            den[ii+1] = div(den[ii+1], n)
+            push!(suite["task"][threadid()], threadinfo)
         end
     end
     n_result = 0
