bd041fff01bd604c15a9bf1094cf73ea3998d42d
dianabarros <diana.ab@live.com>
2022-05-24 00:12:50 -0300

    fix

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 for i in 2 4 8 16; do
     echo "Running Mutual Friendly Numbers with $i threads.";
-    julia -t $i mutually_friendly_number/benchmarks.jl
+    julia -t $i mutually_friendly_numbers/benchmarks.jl;
 done;
 for i in 2 4 8 16; do
     echo "Running Password Cracking with $i threads.";
-    julia -t $i password_cracking/benchmarks.jl
+    julia -t $i password_cracking/benchmarks.jl;
 done;
