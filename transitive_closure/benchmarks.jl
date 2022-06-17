1afe436a63886fe847db1613fe3ed1c255a556ae
dianabarros <diana.ab@live.com>
2022-06-17 18:36:08 -0300

    Cleaning up memory for metrics

--- transitive_closure/benchmarks.jl
+++ transitive_closure/benchmarks.jl
@@ -36,14 +36,15 @@ end
 
 iterations = 10
 
-df = DataFrame(func=String[], input=String[], executor=Vector{Union{String,Missing}}(), n_threads=Int64[], 
-                basesize=Vector{Union{Int64,Missing}}(),total_bytes=Int64[], total_time=Float64[])
-df_file_name = string("transitive_closure_results_",nthreads(),".csv")
 
 run_counter = 1
 for run in runs
+    df = DataFrame(func=String[], input=String[], executor=Vector{Union{String,Missing}}(), n_threads=Int64[], 
+                basesize=Vector{Union{Int64,Missing}}(),total_bytes=Int64[], total_time=Float64[])
+    df_file_name = string("transitive_closure_results_",nthreads(), "_run_", run_counter,".csv")
     it_dist = Dict()
     it_ttime = Dict()
+    GC.gc() # Forcing gc for getting metrics
     for it in 1:iterations
         println("run = ", (f=run.f, nNodes=run.nNodes, bytes_per_row=run.bytes_per_row, ex=run.ex, basesize=run.basesize, check_sequential=run.check_sequential)) 
         bench_sample = debug(
@@ -63,6 +64,7 @@ for run in runs
             total_bytes=bench_sample.suite["app"].bytes, total_time=bench_sample.suite["app"].time))
         CSV.write(df_file_name, df)
     end
+    GC.gc() # Forcing gc for getting metrics
 
     open(string("transitive_closure_task_distribution_",nthreads(), "_", run_counter,".txt"), "w") do io
         print(io, (run=run, dist=it_dist))
