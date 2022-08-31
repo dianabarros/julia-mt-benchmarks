151c8d7d3e2bb479c8f33dd36281d25362f93a75
dianabarros <diana.ab@live.com>
2022-08-30 21:54:52 -0300

    fix profile

--- mutually_friendly_numbers/profile/profile.sh
+++ mutually_friendly_numbers/profile/profile.sh
@@ -1,21 +1,21 @@
 for i in {1..10}; do
-    command time -v ../fsm 0 50000 > small_seq_log_${i}.txt
-    # command time -v ../fsm 0 200000 > medium_seq_log_${i}.txt
-    # command time -v ../fsm 0 350000 > large_seq_log_${i}.txt
+    command time -v --output=small_seq_log_${i}.txt ../fsm 0 50000 
+    command time -v --output=medium_seq_log_${i}.txt ../fsm 0 200000
+    command time -v --output=large_seq_log_${i}.txt ../fsm 0 350000
 
-    command time -v OMP_NUM_THREADS=2 ../fm 0 50000 > small_mt_2_log_${i}.txt
-    # command time -v OMP_NUM_THREADS=2 ../fm 0 200000 > medium_mt_2_log_${i}.txt
-    # command time -v OMP_NUM_THREADS=2 ../fm 0 350000 > large_mt_2_log_${i}.txt
+    OMP_NUM_THREADS=2 command time -v --output=small_mt_2_log_${i}.txt ../fm 0 50000
+    OMP_NUM_THREADS=2 command time -v --output=medium_mt_2_log_${i}.txt ../fm 0 200000
+    OMP_NUM_THREADS=2 command time -v --output=large_mt_2_log_${i}.txt ../fm 0 350000
 
-    command time -v OMP_NUM_THREADS=4 ../fm 0 50000 > small_mt_4_log_${i}.txt
-    command time -v OMP_NUM_THREADS=4 ../fm 0 200000 > medium_mt_4_log_${i}.txt
-    command time -v OMP_NUM_THREADS=4 ../fm 0 350000 > large_mt_4_log_${i}.txt
+    OMP_NUM_THREADS=4 command time -v --output=small_mt_4_log_${i}.txt ../fm 0 50000
+    OMP_NUM_THREADS=4 command time -v --output=medium_mt_4_log_${i}.txt ../fm 0 200000
+    OMP_NUM_THREADS=4 command time -v --output=large_mt_4_log_${i}.txt ../fm 0 350000
 
-    command time -v OMP_NUM_THREADS=8 ../fm 0 50000 > small_mt_8_log_${i}.txt
-    command time -v OMP_NUM_THREADS=8 ../fm 0 200000 > medium_mt_8_log_${i}.txt
-    command time -v OMP_NUM_THREADS=8 ../fm 0 350000 > large_mt_8_log_${i}.txt
+    OMP_NUM_THREADS=8 command time -v --output=small_mt_8_log_${i}.txt ../fm 0 50000
+    OMP_NUM_THREADS=8 command time -v --output=medium_mt_8_log_${i}.txt ../fm 0 200000
+    OMP_NUM_THREADS=8 command time -v --output=large_mt_8_log_${i}.txt ../fm 0 350000
 
-    command time -v OMP_NUM_THREADS=16 ../fm 0 50000 > small_mt_16_log_${i}.txt
-    command time -v OMP_NUM_THREADS=16 ../fm 0 200000 > medium_mt_16_log_${i}.txt
-    command time -v OMP_NUM_THREADS=16 ../fm 0 350000 > large_mt_16_log_${i}.txt
+    OMP_NUM_THREADS=16 command time -v --output=small_mt_16_log_${i}.txt ../fm 0 50000
+    OMP_NUM_THREADS=16 command time -v --output=medium_mt_16_log_${i}.txt ../fm 0 200000
+    OMP_NUM_THREADS=16 command time -v --output=large_mt_16_log_${i}.txt ../fm 0 350000
 done;
