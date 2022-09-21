907997f146983852562e14fe1729a837b7df6461
dianabarros <diana.ab@live.com>
2022-09-21 00:07:33 -0300

    update

--- password_cracking/profile/time.sh
+++ password_cracking/profile/time.sh
@@ -1,30 +1,30 @@
 echo "Compiling...";
-gcc ../brute_force_password_cracking_seq_time.c -lssl -lcrypto -o bfst
-gcc ../brute_force_password_cracking_time.c -lssl -lcrypto -o bft -fopenmp
+gcc ../brute_force_password_cracking_seq_time.c -lssl -lcrypto -o bfst &&
+gcc ../brute_force_password_cracking_time.c -lssl -lcrypto -o bft -fopenmp &&
 
 for i in {1..10}; do
     echo "Running sequential version"
-    ../bfst be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_seq.txt
-    ../bfst 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_seq.txt
-    ../bfst 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_seq.txt
+    ./bfst be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_seq.txt
+    ./bfst 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_seq.txt
+    ./bfst 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_seq.txt
 
     echo "Running OpenMP version with 2 threads"
-    OMP_NUM_THREADS=2 ../bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_2.txt
-    OMP_NUM_THREADS=2 ../bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_2.txt
-    OMP_NUM_THREADS=2 ../bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_2.txt
+    OMP_NUM_THREADS=2 ./bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_2.txt
+    OMP_NUM_THREADS=2 ./bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_2.txt
+    OMP_NUM_THREADS=2 ./bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_2.txt
 
     echo "Running OpenMP version with 4 threads"
-    OMP_NUM_THREADS=4 ../bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_4.txt
-    OMP_NUM_THREADS=4 ../bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_4.txt
-    OMP_NUM_THREADS=4 ../bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_4.txt
+    OMP_NUM_THREADS=4 ./bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_4.txt
+    OMP_NUM_THREADS=4 ./bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_4.txt
+    OMP_NUM_THREADS=4 ./bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_4.txt
 
     echo "Running OpenMP version with 8 threads"
-    OMP_NUM_THREADS=8 ../bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_8.txt
-    OMP_NUM_THREADS=8 ../bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_8.txt
-    OMP_NUM_THREADS=8 ../bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_8.txt
+    OMP_NUM_THREADS=8 ./bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_8.txt
+    OMP_NUM_THREADS=8 ./bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_8.txt
+    OMP_NUM_THREADS=8 ./bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_8.txt
 
     echo "Running OpenMP version with 16 threads"
-    OMP_NUM_THREADS=16 ../bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_16.txt
-    OMP_NUM_THREADS=16 ../bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_16.txt
-    OMP_NUM_THREADS=16 ../bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_16.txt
+    OMP_NUM_THREADS=16 ./bft be5d75fa67ef370e98b3d3611c318156 >> time_logs/small_mt_16.txt
+    OMP_NUM_THREADS=16 ./bft 9cbbf96d1973a60adebbb153f64b48f6  >> time_logs/medium_mt_16.txt
+    OMP_NUM_THREADS=16 ./bft 34799a12a6ef24ef95a0f3179ac3c78d >> time_logs/large_mt_16.txt
 done;
--- transitive_closure/profile/time.sh
+++ transitive_closure/profile/time.sh
@@ -1,30 +1,30 @@
 echo "Compiling...";
-gcc ../transitive_closure_seq_time.c -o tcst;
-gcc ../transitive_closure_time.c -o tct;
+gcc ../transitive_closure_seq_time.c -o tcst &&
+gcc ../transitive_closure_time.c -o tct &&
 
 for i in {1..10}; do
     echo "Running sequential version"
-    ../tcst < ../1280_nodes.in >> time_logs/small_seq.txt
-    ../tcst < ../2560_nodes.in >> time_logs/medium_seq.txt
-    ../tcst < ../transitive_closure.in >> time_logs/large_seq.txt
+    ./tcst < ../1280_nodes.in >> time_logs/small_seq.txt
+    ./tcst < ../2560_nodes.in >> time_logs/medium_seq.txt
+    ./tcst < ../transitive_closure.in >> time_logs/large_seq.txt
 
     echo "Running OpenMP version with 2 threads"
-    OMP_NUM_THREADS=2 ../tct < ../1280_nodes.in >> time_logs/small_mt_2.txt
-    OMP_NUM_THREADS=2 ../tct < ../2560_nodes.in >> time_logs/medium_mt_2.txt
-    OMP_NUM_THREADS=2 ../tct < ../transitive_closure.in >> time_logs/large_mt_2.txt
+    OMP_NUM_THREADS=2 ./tct < ../1280_nodes.in >> time_logs/small_mt_2.txt
+    OMP_NUM_THREADS=2 ./tct < ../2560_nodes.in >> time_logs/medium_mt_2.txt
+    OMP_NUM_THREADS=2 ./tct < ../transitive_closure.in >> time_logs/large_mt_2.txt
 
     echo "Running OpenMP version with 4 threads"
-    OMP_NUM_THREADS=4 ../tct < ../1280_nodes.in >> time_logs/small_mt_4.txt
-    OMP_NUM_THREADS=4 ../tct < ../2560_nodes.in >> time_logs/medium_mt_4.txt
-    OMP_NUM_THREADS=4 ../tct < ../transitive_closure.in >> time_logs/large_mt_4.txt
+    OMP_NUM_THREADS=4 ./tct < ../1280_nodes.in >> time_logs/small_mt_4.txt
+    OMP_NUM_THREADS=4 ./tct < ../2560_nodes.in >> time_logs/medium_mt_4.txt
+    OMP_NUM_THREADS=4 ./tct < ../transitive_closure.in >> time_logs/large_mt_4.txt
 
     echo "Running OpenMP version with 8 threads"
-    OMP_NUM_THREADS=8 ../tct < ../1280_nodes.in >> time_logs/small_mt_8.txt
-    OMP_NUM_THREADS=8 ../tct < ../2560_nodes.in >> time_logs/medium_mt_8.txt
-    OMP_NUM_THREADS=8 ../tct < ../transitive_closure.in >> time_logs/large_mt_8.txt
+    OMP_NUM_THREADS=8 ./tct < ../1280_nodes.in >> time_logs/small_mt_8.txt
+    OMP_NUM_THREADS=8 ./tct < ../2560_nodes.in >> time_logs/medium_mt_8.txt
+    OMP_NUM_THREADS=8 ./tct < ../transitive_closure.in >> time_logs/large_mt_8.txt
 
     echo "Running OpenMP version with 16 threads"
-    OMP_NUM_THREADS=16 ../tct < ../1280_nodes.in >> time_logs/small_mt_16.txt
-    OMP_NUM_THREADS=16 ../tct < ../2560_nodes.in >> time_logs/medium_mt_16.txt
-    OMP_NUM_THREADS=16 ../tct < ../transitive_closure.in >> time_logs/large_mt_16.txt
+    OMP_NUM_THREADS=16 ./tct < ../1280_nodes.in >> time_logs/small_mt_16.txt
+    OMP_NUM_THREADS=16 ./tct < ../2560_nodes.in >> time_logs/medium_mt_16.txt
+    OMP_NUM_THREADS=16 ./tct < ../transitive_closure.in >> time_logs/large_mt_16.txt
 done;
