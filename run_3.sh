61628356fe1940451d4f60f4bcc411fd5ce518b7
dianabarros <diana.ab@live.com>
2022-12-20 11:01:38 -0300

    spliting runs

--- run_3.sh
+++ run_3.sh
@@ -1,8 +1,8 @@
 start_time=$(date +%s);
 julia instantiate.jl;
 for i in 2 4 8 16 32 64; do
-    echo "Running Password Cracking with $i threads.";
-    julia -t $i password_cracking/benchmarks.jl --timed;
+    echo "Running Transitive Closure with $i threads.";
+    julia -t $i transitive_closure/benchmarks.jl --timed; #--funcs [debug_warshall_threads!,debug_warshall!];
 done;
 end_time=$(date +%s);
 elapsed=$(( end_time - start_time ));
