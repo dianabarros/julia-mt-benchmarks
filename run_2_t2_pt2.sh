87d10c5c0f71f4c32dfef7dac0a5e2688bb7a006
dianabarros <diana.ab@live.com>
2023-01-03 12:41:47 -0300

    Splitting password cracking runs

---
+++ run_2_t2_pt2.sh
@@ -0,0 +1,7 @@
+start_time=$(date +%s);
+julia instantiate.jl;
+echo "Running Password Cracking with 2 threads.";
+julia -t 2 password_cracking/benchmarks.jl --timed --inputs [6];
+end_time=$(date +%s);
+elapsed=$(( end_time - start_time ));
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')";
