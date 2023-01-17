2d2953a4b28b7ecdfdcc2cd53922a70fb30c6445
dianabarros <diana.ab@live.com>
2023-01-17 01:00:39 -0300

    Synth apps plotting

--- run5/synthetic_apps/plotting.jl
+++ run5/synthetic_apps/plotting.jl
@@ -15,25 +15,28 @@ gb = groupby(df, [:func, :input, :n_threads])
 df = combine(gb, 
     [:main_loop_time, :imbalance] .=>  mean, [:main_loop_time, :imbalance] .=> std)
 
-seq_df = vcat(df[df.func .== "balanced"], df[df.func .== "unbalanced"])
+seq_df = vcat(df[df.func .== "balanced", :], df[df.func .== "unbalanced", :])
 parallel_df = vcat(
-    df[df.func .== "balanced_mt"], df[df.func .== "balanced_spawn"],
-    df[df.func .== "unbalanced_mt"], df[df.func .== "unbalanced_spawn"]
+    df[df.func .== "balanced_mt", :], df[df.func .== "balanced_spawn", :],
+    df[df.func .== "unbalanced_mt", :], df[df.func .== "unbalanced_spawn", :]
 )
 
-parallel_seq_df = innerjoin(parallel_df, seq_df, on=[:input, :n_threads], renamecols= "_parallel" => "_seq")
+parallel_seq_df = innerjoin(parallel_df, seq_df, on=[:input], renamecols= "_parallel" => "_seq")
 parallel_seq_df = hcat(parallel_seq_df, DataFrame(speedup=Vector{Union{Missing, Float64}}(missing,size(parallel_seq_df,1))))
 
 speedup_df = select(parallel_seq_df, :, [:main_loop_time_mean_parallel, :main_loop_time_mean_seq] => ((main_loop_time_mean_parallel, main_loop_time_mean_seq) -> (main_loop_time_mean_seq./main_loop_time_mean_parallel)) => :speedup)
 
+# TODO: change size?
 speedup_plot = speedup_df |>
     @vlplot(
-        mark={:line, clip=true},
-        x="n_threads:q",
+        mark={:bar, clip=true},
+        x="n_threads_parallel:n",
         y={:speedup},
-        color=:func,
-        column=:input
-        )
+        color=:func_parallel,
+        column=:func_parallel,
+        row=:input
+    )
+speedup_plot |> save("$(run_)/$(app)/speedup_plot.png")
 
 imbalance_plot = parallel_df |>
     @vlplot(
@@ -43,3 +46,4 @@ imbalance_plot = parallel_df |>
         color=:func,
         column=:input
         )
+imbalance_plot |> save("$(run_)/$(app)/imbalance_plot.png")
