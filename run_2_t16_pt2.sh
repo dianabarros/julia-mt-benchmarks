e3a538dfd8ece6d9cd26cd985467923383dff39c
dianabarros <diana.ab@live.com>
2023-01-25 14:48:44 -0300

    Changing number os iterations

--- run_2_t16_pt2.sh
+++ run_2_t16_pt2.sh
@@ -1,7 +1,7 @@
 start_time=$(date +%s);
 julia instantiate.jl;
 echo "Running Password Cracking with 16 threads.";
-julia -t 16 password_cracking/benchmarks.jl --timed --inputs [6];
+julia -t 16 password_cracking/benchmarks.jl --timed --inputs [6] --its 1;
 end_time=$(date +%s);
 elapsed=$(( end_time - start_time ));
 eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')";
--- run_2_t8_pt2.sh
+++ run_2_t8_pt2.sh
@@ -1,7 +1,7 @@
 start_time=$(date +%s);
 julia instantiate.jl;
 echo "Running Password Cracking with 8 threads.";
-julia -t 8 password_cracking/benchmarks.jl --timed --inputs [6];
+julia -t 8 password_cracking/benchmarks.jl --timed --inputs [6] --its 1;
 end_time=$(date +%s);
 elapsed=$(( end_time - start_time ));
 eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')";
